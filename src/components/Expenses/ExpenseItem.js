c5caf7ed753fe45ae538f61eef76e49ab8bf3742
Dev <devme619@gmail.com>
2022-06-28 22:59:02 +0530

    used useState to upadte component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,23 @@
-import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css';
-import Card from'../UI/Card'
-function ExpenseItem(props) {
-    
-    return (
-      <Card className="expense-item">
-        <ExpenseDate date={props.date}/>
-        <div className="expense-item__description">
-          <h2>{props.title}</h2>
-          <div className="expense-item__price">Rs.{props.amount}</div>
-        </div>
-      </Card>
-    );
-    
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+const ExpenseItem=(props) =>{
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  };
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">Rs.{props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
